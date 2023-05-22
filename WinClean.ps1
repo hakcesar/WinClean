e85bd3d8bfb31481a7f40438d22a69ff6578bcf4
hakcesar <102752831+hakcesar@users.noreply.github.com>
2023-05-22 00:07:07 -0400

    WinClean.ps1

--- WinClean.ps1
+++ WinClean.ps1
@@ -10,40 +10,52 @@ if ($confirmation -ne "Y") {
     exit
 }
 
+# Function to disable Microsoft Edge startup impact
+function Disable-MicrosoftEdgeStartupImpact {
+    Write-Host "Disabling Microsoft Edge startup impact..."
+
+    $edgeExclusion = "Microsoft.MicrosoftEdge"
+    $edgeUpdateExclusion = "Microsoft.MicrosoftEdge.Update"
+
+    $edgeStartupStatus = (Get-MpPreference).ExclusionProcess | Where-Object { $_ -eq $edgeExclusion }
+    if ($edgeStartupStatus) {
+        Write-Host "Microsoft Edge startup impact is already disabled."
+    }
+    else {
+        $exclusionProcesses = (Get-MpPreference).ExclusionProcess
+        $exclusionProcesses += $edgeExclusion
+        $exclusionProcesses += $edgeUpdateExclusion
+        Set-MpPreference -ExclusionProcess $exclusionProcesses
+        Write-Host "Microsoft Edge startup impact disabled."
+    }
+}
+
 # Disable common program startup impact
 Write-Host "Disabling program startup impact..."
 
 # Disable Microsoft Edge
-$edgeExclusion = "Microsoft.MicrosoftEdge"
-$edgeUpdateExclusion = "Microsoft.MicrosoftEdge.Update"
-
-$edgeStartupStatus = (Get-MpPreference).ExclusionProcess | Where-Object { $_ -eq $edgeExclusion }
-if ($edgeStartupStatus) {
-    Write-Host "Microsoft Edge startup impact is already disabled."
-}
-else {
-    Set-MpPreference -ExclusionProcess ($edgeExclusion, $edgeUpdateExclusion)
-    Write-Host "Microsoft Edge startup impact disabled."
-}
+Disable-MicrosoftEdgeStartupImpact
 
 Write-Host "Program startup impact disabled."
+
+Start-Sleep -Seconds 5
 
 # Clean up temporary files
 Write-Host "Cleaning up temporary files..."
 Remove-Item -Path $env:TEMP\* -Force -Recurse
 Write-Host "Temporary files cleaned."
-Start-Sleep -Seconds 2
+Start-Sleep -Seconds 5
 
 # Clear the recycle bin
 Write-Host "Clearing the recycle bin..."
 Clear-RecycleBin -Force -ErrorAction SilentlyContinue
-Start-Sleep -Seconds 2
+Start-Sleep -Seconds 5
 
 # Perform disk cleanup of system files
 Write-Host "Performing disk cleanup..."
 Start-Process -FilePath "cleanmgr.exe" -ArgumentList "/sagerun:1" -Wait
 Write-Host "Disk cleanup of system files completed."
-Start-Sleep -Seconds 2
+Start-Sleep -Seconds 5
 
 
 # Uninstall unwanted or unnecessary programs
